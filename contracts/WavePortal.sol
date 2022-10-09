5376c2f3866716a2a3eb8728fea40aed6ee9f707
Kevin Sun <33167525+sunkevin265-coder@users.noreply.github.com>
2022-10-08 20:10:35 -0500

    Added waving functionality

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,19 @@ pragma solidity ^0.8.17;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves; // This is known as a state variable since it maintains state
+
     constructor() {
         console.log("Yo yo, I am a contract and I am smart");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender); // msg.sender is the wallet address of the person who called the function
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,18 @@
 const main = async () => {
+    const [owner, randomPerson] = await hre.ethers.getSigners();
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
     const waveContract = await waveContractFactory.deploy();
     await waveContract.deployed();
+
     console.log("Contract deployed to:", waveContract.address);
+    console.log("Contract deployed by:", owner.address);
+
+    await waveContract.getTotalWaves();
+
+    const waveTxn = await waveContract.wave();
+    await waveTxn.wait();
+
+    await waveContract.getTotalWaves();
 };
   
 const runMain = async () => {
